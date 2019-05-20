1d2080023ce70f4bc3d68f3a3b0b93403c6bc123
Michael Hutcheon <michael.hutcheon@hotmail.co.uk>
2019-05-20 15:09:03 +0100

    Turns out the factor of 2 was in the analysis scripts. I was fitting <r>_\psi^2 rather than <r>_\psi

--- fit_hydrogen_electrons.py
+++ fit_hydrogen_electrons.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt;
+import sys
 import numpy as np
 
 from scipy.optimize import curve_fit
@@ -6,17 +7,21 @@ from scipy.optimize import curve_fit
 # Attempts to fit a hydrogen 1s ground state to the electron
 # wavefunction sampled in the "electrons" file.
 
-# The radial probability density of a 1s state
-# including the r^2 prefactor (parameterised)
+# The radial probability of a DMC walker in the
+# hydrogen ground state. The walkers will sample
+# the distribution \psi_0 = np.exp(-r/a), but a 
+# phase space factor of r^2 also appears.
 def rad_prob(r,a,b):
-	return b*r*r*np.exp(-2*r/a)/np.exp(-2)
+	return b*r*r*np.exp(-r/a)
 
 # The maximum radius we consider
 MAX_R = 20
 
 # read in the radii from the "electrons" file
+to_open = "electrons"
+if len(sys.argv) > 1: to_open = sys.argv[1]
 rs = []
-for line in [l for l in open("electrons").read().split("\n") if len(l) > 0]:
+for line in [l for l in open(to_open).read().split("\n") if len(l) > 0]:
 	x = np.array([float(i) for i in line.split(",")])
 	r = np.linalg.norm(x)
 	if r < MAX_R:
@@ -37,7 +42,7 @@ lab += str(round(np.sqrt(covar[0][0]), 5))
 plt.plot(rgrid, yfit, label=lab)
 y1s = rad_prob(rgrid, 1, 1)
 y1s *= max(yfit)/max(y1s)
-plt.plot(rgrid, y1s,label="Hydrogen true 1s: a0 = 1")
+plt.plot(rgrid, y1s,label="Hydrogen true 1s: a0 = 1", linestyle=":")
 plt.xlabel("Radius (atomic units)")
 plt.ylabel("Counts")
 plt.legend(loc="best")
