deeb1b8ec5402374476be791597d39fd5fb8e20a
Michael Hutcheon <michael.hutcheon@hotmail.co.uk>
2019-05-20 13:58:45 +0100

    Made build script more cross-platform

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 # Build the program
-mpiCC -O3 -o dmc main.cpp 
+
+# Find a working copy of mpiCC
+mpiccs=("mpiCC" "mpicc")
+for cc in "${mpiccs[@]}"
+do
+	found=$(which $cc)
+	if [ ! -z "$found" ]; then
+		MPI_CC=$found
+		break
+	fi
+done
+
+$MPI_CC -O3 -o dmc main.cpp 
