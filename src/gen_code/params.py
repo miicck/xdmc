15137df4216cca394403ebd2e2642ad9e8b0fe00
miicck <mjh261@cam.ac.uk>
2019-08-27 11:13:19 +0100

    Tidied parameter generation code

--- src/gen_code/params.py
+++ src/gen_code/params.py
@@ -1,190 +1,166 @@
 # This file acts as a central source for all of the parameter definitions. It is
 # used to generate the parameters in the cpp code, generate documentation and to
 # generate automatic parameter parsing/output code.
-#
-# The syntax for a parameter is
-#
-#   params["name_read_from_input_file"] = [
-#     "cpp_datatype",               # String - directly inserted into c++ file(s)
-#     "cpp_variable_name",          # String - directly inserted into c++ file(s)
-#     "default_value",              # String - directly inserted into c++ file(s)
-#     "description",                # String - directly inserted into c++ file(s)
-#      is_read_from_input_file      # Bool   - used to inform code generation
-#   ]
-#
+# The order of parameter definitions here will be the order they are read in and
+# output.
 
-params = {}
-
-params["pid"] = [
-    "int",
-    "pid",
-    "0",
-    "The MPI process id of this process. Will be in [0,np).",
-    False
-]
-
-params["np"] = [
-    "int",
-    "np",
-    "1",
-    "The number of MPI processes.",
-    False
-]
-
-params["dimensions"] = [
-    "unsigned",
-    "dimensions",
-    "3",
-    "The spatial dimensions of the system.",
-    True
-]
-
-params["walkers"] = [
-    "unsigned",
-    "target_population",
-    "1000",
-    ("The target population of DMC walkers. The actual number of "
-     "dmc walkers will fluctuate during runtime, but will be bias "
-     "towards this value."),
-     True
-]
-
-params["max_pop_ratio"] = [
-    "double",
-    "max_pop_ratio",
-    "4.0",
-    ("The maximum allowed population, expressed as a multiple of "
-     "the target population."),
-     True
-]
-
-params["min_pop_ratio"] = [
-    "double",
-    "min_pop_ratio",
-    "0.0",
-    ("The minimum allowed population, expressed as a fraction of "
-    "the target population."),
-    True
-]
-
-params["iterations"] = [
-    "unsigned",
-    "dmc_iterations",
-    "10000",
-    ("The number of DMC iterations, each corresponding to a "
-     "step of tau in imaginary time."),
-     True
-]
-
-params["iter"] = [
-    "unsigned",
-    "dmc_iteration",
-    "0",
-    ("The current DMC iteration."),
-    False
-]
-
-params["tau"] = [
-    "double",
-    "tau",
-    "0.01",
-    "The DMC timestep in atomic units.",
-    True
-]
-
-params["tau_c_ratio"] = [
-    "double",
-    "tau_c_ratio",
-    "1.0",
-    ("The ratio of tau_c:tau, where tau_c is the effective "
-     "cancellation timestep and tau is the DMC timestep."),
-     True
-]
-
-params["trial_energy"] = [
-    "double",
-    "trial_energy",
-    "0.0",
-    ("The DMC trial energy in atomic units (Hartree). This "
-     "value is used to control the DMC population and will "
-     "fluctuate during runtime. After equilibriation, it will "
-     "fluctuate around the ground state energy of the system."),
-     True
-]
-
-params["best_energy"] = [
-    "double",
-    "best_energy",
-    "0.0",
-    ("The best estimate of the DMC energy in atomic units (Hartree). "
-     "This will be updated as the simulation progresses."),
-     True
-]
-
-params["pre_diffusion"] = [
-    "double",
-    "pre_diffusion",
-    "1.0",
-    ("The amount of imaginary time that the walkers will diffuse for "
-     "before the first full DMC iteration. Effectively, this is how "
-     "spread out the initial wavefunction is."),
-     True
-]  
-
-params["write_wavefunction"] = [
-   "bool",
-   "write_wavefunction",
-   "true",
-   "True if wavefunction files are to be written.",
-   True
-]
-
-params["exchange_moves"] = [
-    "bool",
-    "exchange_moves",
-    "true",
-    "True if exchange moves are to be made.",
-    True
-]
-
-params["exchange_prob"] = [
-    "double",
-    "exchange_prob",
-    "0.5",
-    ("The probability of a walker making an exchange move in any "
-     "given timestep. The actual exchange move made will be chosen at random. "
-     "1 - this is the probability of simply diffusing, making no exchange moves."),
-     True
-]
-
-params["cancel_scheme"] = [
-    "std::string",
-    "cancel_scheme",
-    '"diffusive"',
-    "The cancellation scheme used.",
-    True
-]
-
-params["cancel_function"] = [
-    "std::string",
-    "cancel_function",
-    '"integrated_weight"',
-    "The cancellation function used if we are applying pairwise cancellations.",
-    True
-]
-
-params["cancelled_weight"] = [
-    "double",
-    "cancelled_weight",
-    "0.0",
-    ("The amount of weight cancelled during the last cancellation step."),
-    False
-]
-
-params["correct_seperations"] = [
-    "bool",
-    "correct_seperations",
-    "false",
-    "True if seperation corrections are applied.",
-    True
-]
+params = [{
+    "in_name"     : "dimensions",
+    "type"        : "unsigned",
+    "cpp_name"    : "dimensions",
+    "default"     : "3",
+    "description" : "The spatial dimensions of the system.",
+    "read_in"     : True
+},{
+    "in_name"     : "walkers",
+    "type"        : "unsigned",
+    "cpp_name"    : "target_population",
+    "default"     : "1000",
+    "description" : ("The target population of DMC walkers. The actual number of "
+                     "dmc walkers will fluctuate during runtime, but will be bias "
+                     "towards this value."),
+    "read_in"     : True
+},{
+    "in_name"     : "max_pop_ratio",
+    "type"        : "double",
+    "cpp_name"    : "max_pop_ratio",
+    "default"     : "4.0",
+    "description" : ("The maximum allowed population, expressed as a multiple of "
+                     "the target population."),
+    "read_in"     : True
+},{
+    "in_name"     : "min_pop_ratio",
+    "type"        : "double",
+    "cpp_name"    : "min_pop_ratio",
+    "default"     : "0.0",
+    "description" : ("The minimum allowed population, expressed as a fraction of "
+                     "the target population."),
+    "read_in"     : True
+},{
+    "in_name"     : "iterations",
+    "type"        : "unsigned",
+    "cpp_name"    : "dmc_iterations",
+    "default"     : "10000",
+    "description" : ("The number of DMC iterations, each corresponding to a "
+                     "step of tau in imaginary time."),
+    "read_in"     : True
+},{
+    "in_name"     : "iter",
+    "type"        : "unsigned",
+    "cpp_name"    : "dmc_iteration",
+    "default"     : "0",
+    "description" : "The current DMC iteration.",
+    "read_in"     : False
+},{
+    "in_name"     : "tau",
+    "type"        : "double",
+    "cpp_name"    : "tau",
+    "default"     : "0.01",
+    "description" : "The DMC timestep in atomic units.",
+    "read_in"     : True
+},{
+    "in_name"     : "tau_c_ratio",
+    "type"        : "double",
+    "cpp_name"    : "tau_c_ratio",
+    "default"     : "1.0",
+    "description" : ("The ratio of tau_c:tau, where tau_c is the effective "
+                     "cancellation timestep and tau is the DMC timestep."),
+    "read_in"     : True
+},{
+    "in_name"     : "np",
+    "type"        : "int",
+    "cpp_name"    : "np",
+    "default"     : "1",
+    "description" : "The number of MPI processes.",
+    "read_in"     : False
+},{
+    "in_name"     : "pid",
+    "type"        : "int",
+    "cpp_name"    : "pid",
+    "default"     : "0",
+    "description" : "The MPI process id of this process. Will be in [0,np).",
+    "read_in"     : False
+},{
+    "in_name"     : "trial_energy",
+    "type"        : "double",
+    "cpp_name"    : "trial_energy",
+    "default"     : "0.0",
+    "description" : ("The DMC trial energy in atomic units (Hartree). This "
+                     "value is used to control the DMC population and will "
+                     "fluctuate during runtime. After equilibriation, it will "
+                     "fluctuate around the ground state energy of the system."),
+     "read_in"    : True
+},{
+    "in_name"     : "pre_diffusion",
+    "type"        : "double",
+    "cpp_name"    : "pre_diffusion",
+    "default"     : "1.0",
+    "description" : ("The amount of imaginary time that the walkers will diffuse for "
+                     "before the first full DMC iteration. Effectively, this is how "
+                     "spread out the initial wavefunction is."),
+    "read_in"     : True
+},{
+    "in_name"     : "write_wavefunction",
+    "type"        : "bool",
+    "cpp_name"    : "write_wavefunction",
+    "default"     : "true",
+    "description" : "True if wavefunction files are to be written.",
+    "read_in"     : True
+},{
+    "in_name"     : "exchange_moves",
+    "type"        : "bool",
+    "cpp_name"    : "exchange_moves",
+    "default"     : "true",
+    "description" : "True if exchange moves are to be made.",
+    "read_in"     : True
+},{
+    "in_name"     : "exchange_prob",
+    "type"        : "double",
+    "cpp_name"    : "exchange_prob",
+    "default"     : "0.5",
+    "description" : ("The probability of a walker making an exchange move in any "
+                     "given timestep. The actual exchange move made will be chosen "
+                     "at random. 1 - this is the probability of simply diffusing, "
+                     "making no exchange moves."),
+    "read_in"     : True
+},{
+    "in_name"     : "cancel_scheme",
+    "type"        : "std::string",
+    "cpp_name"    : "cancel_scheme",
+    "default"     : '"diffusive"',
+    "description" : "The cancellation scheme used.",
+    "read_in"     : True
+},{
+    "in_name"     : "cancel_function",
+    "type"        : "std::string",
+    "cpp_name"    : "cancel_function",
+    "default"     : '"integrated_weight"',
+    "description" : "The cancellation function used if we are applying pairwise cancellations.",
+    "read_in"     : True
+},{
+    "in_name"     : "cancelled_weight",
+    "type"        : "double",
+    "cpp_name"    : "cancelled_weight",
+    "default"     : "0.0",
+    "description" : "The amount of weight cancelled during the last cancellation step.",
+    "read_in"     : False
+},{
+    "in_name"     : "correct_seperations",
+    "type"        : "bool",
+    "cpp_name"    : "correct_seperations",
+    "default"     : "false",
+    "description" : "True if seperation corrections are applied.",
+    "read_in"     : True
+},{
+    "in_name"     : "self_sign_strength",
+    "type"        : "double",
+    "cpp_name"    : "self_sign_strength",
+    "default"     : "0.0",
+    "description" : ("The strength of a walkers own greens function in "
+                     "deciding what sign it should be. A value of 0 strongly "
+                     "encourages the formation of nodal pockets, but represents "
+                     "an approximation to the true greens function. A value of " 
+                     "1 reproduces the correct greens function but is likely to "
+                     "lead to nodal pockets breaking apart."),
+    "read_in"     : True
+}]
