101935207852785d99a78066c5830bda35141d49
miicck <mjh261@cam.ac.uk>
2019-12-01 18:17:31 +0000

    Parallelized build system

---
+++ build.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+import src.gen_code.gen_params # This will generate code
+import os
+
+# Flags controlling build
+COMPILERS     = "mpic++"
+COMPILE_FLAGS = "-c -Wall -g -O3"
+LINK_FLAGS    = "-o xdmc"
+LIBS          = "-lstdc++ -lm"
+
+try:
+    # Attempt to import multiprocessing
+    from multiprocessing import cpu_count, Process
+    from time import sleep
+
+except ImportError:
+    # Fallback to spoof serial case
+    cpu_count = lambda : 1
+    sleep = lambda t : 0
+    class Process:
+        def __init__(self, target=None, args=None):
+            target(*args)
+        def start(self):
+            return 0
+        def join(self):
+            return 0
+        def is_alive(self):
+            return False
+
+def compile_cpp(cpp):
+    global COMPILER, COMPILE_FLAGS
+
+    # Get the relative paths to the .cpp file and
+    # the .o file we want to create/update
+    ofile = "build/"+cpp.replace(".cpp",".o")
+    cfile = "src/"+cpp
+
+    # Check if we need to compile this file
+    if os.path.isfile(ofile):
+        if os.stat(ofile).st_mtime > os.stat(cfile).st_mtime:
+            # ofile is more recent than cfile
+            # => don't need to recompile
+            print(ofile+" already up-to-date")
+            return
+
+    # Compile a cpp file with the given filename in src/
+    # to an object file in build/
+    cmd = COMPILER + " " + COMPILE_FLAGS + " {0} -o {1}"
+    cmd = cmd.format(cfile, ofile)
+    print(cmd)
+    os.system(cmd)
+
+print("\nCompiling on {0} cores...".format(cpu_count()))
+
+# Create the build directory
+if not os.path.exists("build"): os.mkdir("build")
+    
+# Get the cpp files to compile
+cpp_files = [f for f in os.listdir("src/") if f.endswith(".cpp")]
+
+# Compile the c++ files
+procs = []
+for cpp in cpp_files:
+
+    # Wait for a process to become available
+    while len(procs) >= cpu_count():
+        for p in procs:
+            if not p.is_alive():
+                procs.remove(p)
+        # Wait before checking procs again
+        sleep(0.01) 
+
+    p = Process(target=compile_cpp, args=(cpp,))
+    p.start()
+    procs.append(p)
+
+# Finish the compilation
+for p in procs: p.join()
+
+# Check if we need to re-link the executables
+o_files = ["build/"+f for f in os.listdir("build/") if f.endswith(".o")]
+link_exe = True
+if os.path.isfile("xdmc"):
+    link_exe = False
+    for ofile in o_files:
+        if os.stat("xdmc").st_mtime < os.stat(ofile).st_mtime:
+            link_exe = True
+            break
+
+print("\nLinking .o files to xdmc executable...")
+if link_exe:
+    # Link the object files to make the executable
+    cmd = COMPILER + " " + LINK_FLAGS + " " + " ".join(o_files) + " " + LIBS
+    print(cmd)
+    os.system(cmd)
+else:
+    print("xdmc executable is up-to-date.")
--- src/gen_code/gen_params.py
+++ src/gen_code/gen_params.py
@@ -15,15 +15,9 @@
 #     For a copy of the GNU General Public License see <https://www.gnu.org/licenses/>.
 # 
 from params import params
+import os
 # Will generate the params.h and params.cpp files from 
 # params_template.h and params_template.cpp respectively
-
-# The warning message displayed at the top of generated files
-warning  = "// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n"
-warning += "// GENERATED FILE - DO NOT EDIT \n"
-warning += "// Generated from {0} \n"
-warning += "// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n"
-    
 
 # Generate the line(s) of c++ that test the given
 # named parameter according to the given test.
@@ -98,130 +92,171 @@ def gen_test_fail_error(param, test, ws=""):
     raise ValueError("Unkown test: "+test)
 
 # Generate the params.h file from params_template.h
-with open("params_template.h") as f: 
-    lines = f.read().split("\n")
+def gen_params_h():
 
-with open("../params.h", "w") as f:
+    # The warning message displayed at the top of generated files
+    warning  = "// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n"
+    warning += "// GENERATED FILE - DO NOT EDIT \n"
+    warning += "// Generated from {0} \n"
+    warning += "// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n"
 
-    # Write warning at top of file
-    f.write(warning.format("gen_code/params_template.h"))
+    with open("params_template.h") as f: 
+        lines = f.read().split("\n")
 
-    for l in lines:
+    with open("../params.h", "w") as f:
 
-        # Declare parameters as extern in params.h
-        if "PYTHON_GEN_PARAMS_HERE" in l:
-            ws = l[0:l.find("P")]
-            for p in params:
-                f.write(ws+"extern {0} {1};\n".format(p["type"], p["cpp_name"]))
-            continue
+        # Write warning at top of file
+        f.write(warning.format("gen_code/params_template.h"))
 
-        f.write(l+"\n")
+        for l in lines:
+
+            # Declare parameters as extern in params.h
+            if "PYTHON_GEN_PARAMS_HERE" in l:
+                ws = l[0:l.find("P")]
+                for p in params:
+                    f.write(ws+"extern {0} {1};\n".format(p["type"], p["cpp_name"]))
+                continue
+
+            f.write(l+"\n")
 
 # Generate the params.cpp file from params_template.cpp
-with open("params_template.cpp") as f:
-    lines = f.read().split("\n")
+def gen_params_cpp():
 
-with open("../params.cpp","w") as f:
+    # The warning message displayed at the top of generated files
+    warning  = "// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n"
+    warning += "// GENERATED FILE - DO NOT EDIT \n"
+    warning += "// Generated from {0} \n"
+    warning += "// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n"
 
-    # Write warning at op of file
-    f.write(warning.format("gen_code/params_template.cpp"))
+    with open("params_template.cpp") as f:
+        lines = f.read().split("\n")
 
-    for l in lines:
+    with open("../params.cpp","w") as f:
 
-        # Define parameters in params.cpp, with default value
-        if "PYTHON_GEN_PARAMS_HERE" in l:
-            ws = l[0:l.find("P")]
-            for p in params:
-                f.write(ws+"{0} params::{1} = {2};\n".format(p["type"],p["cpp_name"],p["default"]))
-            continue
+        # Write warning at op of file
+        f.write(warning.format("gen_code/params_template.cpp"))
 
-        # Parse parameters from their tag name in params.cpp
-        if "PYTHON_PARSE_PARAMS_HERE" in l:
-            ws = l[0:l.find("P")]
-            for p in params:
-                if not "in_name" in p: continue
-                f.write(ws+"// Parse {0}\n".format(p["in_name"]))
-                f.write(ws+'if (tag == "{0}")\n'.format(p["in_name"]))
-                f.write(ws+"{\n")
-                f.write(ws+"    std::stringstream ss(split[1]);\n")
-                f.write(ws+"    ss >> params::{0};\n".format(p["cpp_name"]))
-                if not "allowed" in p:
+        for l in lines:
+
+            # Define parameters in params.cpp, with default value
+            if "PYTHON_GEN_PARAMS_HERE" in l:
+                ws = l[0:l.find("P")]
+                for p in params:
+                    f.write(ws+"{0} params::{1} = {2};\n".format(p["type"],p["cpp_name"],p["default"]))
+                continue
+
+            # Parse parameters from their tag name in params.cpp
+            if "PYTHON_PARSE_PARAMS_HERE" in l:
+                ws = l[0:l.find("P")]
+                for p in params:
+                    if not "in_name" in p: continue
+                    f.write(ws+"// Parse {0}\n".format(p["in_name"]))
+                    f.write(ws+'if (tag == "{0}")\n'.format(p["in_name"]))
+                    f.write(ws+"{\n")
+                    f.write(ws+"    std::stringstream ss(split[1]);\n")
+                    f.write(ws+"    ss >> params::{0};\n".format(p["cpp_name"]))
+                    if not "allowed" in p:
+                        f.write(ws+"    continue;\n")
+                        f.write(ws+"}\n\n")
+                        continue
+                    f.write(gen_test(p["cpp_name"], p["allowed"], ws=ws+"    ")+"\n")
+                    f.write(ws+"    {\n")
+                    f.write(gen_test_fail_error(p["cpp_name"], p["allowed"], ws=ws+"        ")+"\n")
+                    f.write(ws+"        return false;\n")
+                    f.write(ws+"    }\n\n")
                     f.write(ws+"    continue;\n")
                     f.write(ws+"}\n\n")
-                    continue
-                f.write(gen_test(p["cpp_name"], p["allowed"], ws=ws+"    ")+"\n")
-                f.write(ws+"    {\n")
-                f.write(gen_test_fail_error(p["cpp_name"], p["allowed"], ws=ws+"        ")+"\n")
-                f.write(ws+"        return false;\n")
-                f.write(ws+"    }\n\n")
-                f.write(ws+"    continue;\n")
-                f.write(ws+"}\n\n")
-            continue
+                continue
 
-        # Output params in params.cpp
-        if "PYTHON_OUTPUT_PARAMS_HERE" in l:
-            ws = l[0:l.find("P")]
-            max_len = max([len(p["in_name"]) for p in params if "in_name" in p])
-            fs = 'progress_file << "    " << "{0:'+str(max_len)+'} "'
-            for p in params:
-                if not "in_name" in p: continue
-                f.write(ws+fs.format(p["in_name"]))
-                f.write(" << params::{0}".format(p["cpp_name"]))
-                f.write(r' << "\n";'+"\n")
-            continue
+            # Output params in params.cpp
+            if "PYTHON_OUTPUT_PARAMS_HERE" in l:
+                ws = l[0:l.find("P")]
+                max_len = max([len(p["in_name"]) for p in params if "in_name" in p])
+                fs = 'progress_file << "    " << "{0:'+str(max_len)+'} "'
+                for p in params:
+                    if not "in_name" in p: continue
+                    f.write(ws+fs.format(p["in_name"]))
+                    f.write(" << params::{0}".format(p["cpp_name"]))
+                    f.write(r' << "\n";'+"\n")
+                continue
 
-        # Output usage info
-        if "PYTHON_GEN_USAGE_INFO_HERE" in l:
+            # Output usage info
+            if "PYTHON_GEN_USAGE_INFO_HERE" in l:
 
-            # Snippets of code to be formatted with information
-            WIDTH     = 60
-            DES_WIDTH = WIDTH - len("Description   : ")
-            ws = l[0:l.find("P")]
-            seperator    = "".join(["_" for i in range(0, WIDTH)])
-            seperator    = r'std::cout << "{0}\n";'.format(seperator) + '\n' 
-            title        = r'std::cout << "Parameter     = {0}\n";' + '\n'
-            type_line    = r'std::cout << "Type          = {0}\n";' + '\n'
-            default_line = r'std::cout << "Default value = {0}\n";' + '\n'
-            description  = r'std::cout << "Description   = {0}\n";' + '\n'
-            new_line     = r'std::cout << "                {0}\n";' + '\n'
-            allowed_line = r'std::cout << "Allowed value(s):\n";' + '\n'
-            blank_line   = r'std::cout << "\n";' + '\n'
+                # Snippets of code to be formatted with information
+                WIDTH     = 60
+                DES_WIDTH = WIDTH - len("Description   : ")
+                ws = l[0:l.find("P")]
+                seperator    = "".join(["_" for i in range(0, WIDTH)])
+                seperator    = r'std::cout << "{0}\n";'.format(seperator) + '\n' 
+                title        = r'std::cout << "Parameter     = {0}\n";' + '\n'
+                type_line    = r'std::cout << "Type          = {0}\n";' + '\n'
+                default_line = r'std::cout << "Default value = {0}\n";' + '\n'
+                description  = r'std::cout << "Description   = {0}\n";' + '\n'
+                new_line     = r'std::cout << "                {0}\n";' + '\n'
+                allowed_line = r'std::cout << "Allowed value(s):\n";' + '\n'
+                blank_line   = r'std::cout << "\n";' + '\n'
 
-            for p in params:
-                if not "in_name" in p: continue
+                for p in params:
+                    if not "in_name" in p: continue
 
-                # Write the parameter name, type and default value
-                f.write(ws+seperator)
-                f.write(ws+title.format(p["in_name"]))
-                f.write(ws+type_line.format(p["type"]))
-                f.write(ws+default_line.format(p["default"].replace('"','')))
+                    # Write the parameter name, type and default value
+                    f.write(ws+seperator)
+                    f.write(ws+title.format(p["in_name"]))
+                    f.write(ws+type_line.format(p["type"]))
+                    f.write(ws+default_line.format(p["default"].replace('"','')))
 
-                # Write parameter description, keeping to a
-                # sensible line length
-                first_line = True
-                words = p["description"].split()
-                des_line = ""
+                    # Write parameter description, keeping to a
+                    # sensible line length
+                    first_line = True
+                    words = p["description"].split()
+                    des_line = ""
 
-                for i, w in enumerate(words):
-                    des_line += w + " "
-                    if i == len(words) - 1 or len(des_line + words[i+1]) >= DES_WIDTH:
-                        if first_line: 
-                            f.write(ws+description.format(des_line))
-                            first_line = False
-                        else:
-                            f.write(ws+new_line.format(des_line))
-                        des_line = ""
+                    for i, w in enumerate(words):
+                        des_line += w + " "
+                        if i == len(words) - 1 or len(des_line + words[i+1]) >= DES_WIDTH:
+                            if first_line: 
+                                f.write(ws+description.format(des_line))
+                                first_line = False
+                            else:
+                                f.write(ws+new_line.format(des_line))
+                            des_line = ""
 
-                f.write(ws+blank_line)
-                test = "any"
-                if "allowed" in p:
-                    test = p["allowed"]
-                f.write(ws+allowed_line)
-                f.write(gen_test_allowed(test, ws=ws))
+                    f.write(ws+blank_line)
+                    test = "any"
+                    if "allowed" in p:
+                        test = p["allowed"]
+                    f.write(ws+allowed_line)
+                    f.write(gen_test_allowed(test, ws=ws))
 
-            continue
-            
-        f.write(l+"\n")
+                continue
+                
+            f.write(l+"\n")
 
+# Set the working directory to the gen_code dir
+working_dir_before = os.getcwd()
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+print("\nGenerating code...")
 
+gen_cpp = True
+if os.path.isfile("../params.cpp"):
+    gen_cpp = os.stat("params_template.cpp").st_mtime > os.stat("../params.cpp").st_mtime
 
+if gen_cpp:
+    print("Generating params.cpp")
+    gen_params_cpp()
+else:
+    print("params.cpp is up-to-date")
+
+gen_h = True
+if os.path.isfile("../params.h"):
+    gen_h = os.stat("params_template.h").st_mtime > os.stat("../params.h").st_mtime
+
+if gen_h:
+    print("Generating params.h")
+    gen_params_h()
+else:
+    print("params.h is up-to-date")
+
+# Reset the working directory
+os.chdir(working_dir_before)
